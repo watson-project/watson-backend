ec89ea66f21e05a283c57df8df354c2ff458b797
joesphchang <joesphschang@gmail.com>
2022-01-26 15:50:55 -0500

    Fixed Delete testing

--- controllers/articlesController.js
+++ controllers/articlesController.js
@@ -21,6 +21,7 @@ router.get('/', async (req, res, next) => {
 router.get('/:id', async (req, res, next) => {
 	try {
 		const article = await Articles.findById(req.params.id);
+		console.log(article);
 		if (article) {
 			res.status(200).json(article);
 		} else {
