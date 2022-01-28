d351fc6bf65e733bef4e9d3b05a2ff1de1f29c62
spell-landon <spell.landon@gmail.com>
2022-01-28 15:59:57 -0600

    Complete Backend Users

--- db/seed.js
+++ db/seed.js
@@ -1,41 +1,46 @@
 // Requires Models
 const Article = require('../models/Articles');
 const User = require('../models/Users');
+const bcrypt = require('bcrypt');
 
 // Requires Article Seed
-const articleSeed = require ('./seed.json');
+const articleSeed = require('./seed.json');
 const userSeed = require('./seedUser.json');
 
-// Logic to empty database 
+// Logic to empty database
 Article.deleteMany({})
-	.then(() => {
-		console.log('Deleted all the articles!');
-		User.deleteMany({});
-	})
-	.then(() => {
-		console.log('Deleted all the users!');
-	})
-	.then(() => {
-		return userSeed.map((users) => {
-            return User.create({ email: users.email, password: users.password })
-        });
-	})
-	.then((user) => {
-		return User.insertMany(user);
-	})
-	.then((newUser) => {
-		console.log('Created a new user!', newUser);
-		return articleSeed.map((articles) => {
-			return Article.create({ ...articles, owner: newUser._id });
-		});
-	})
-	.then((articles) => {
-		return Article.insertMany(articles);
-	})
-	.then((newArticle) => {
-		console.log('We just created new articles!', newArticle);
-	})
-	.catch(console.error)
-	.finally(() => {
-		process.exit();
-	});
+  .then(() => {
+    console.log('Deleted all the articles!');
+    User.deleteMany({});
+  })
+  .then(() => {
+    console.log('Deleted all the users!');
+  })
+  .then(() => {
+    return Promise.all(
+      userSeed.map(async (users) => {
+        const password = await bcrypt.hash(users.password, 10);
+        return { email: users.email, password: password };
+      })
+    );
+  })
+  .then((user) => {
+    return User.insertMany(user);
+  })
+  .then((newUser) => {
+    console.log('Created a new user!', newUser);
+    return articleSeed.map((articles) => {
+      const random = Math.floor(Math.random() * userSeed.length);
+      return { ...articles, owner: newUser[random]._id };
+    });
+  })
+  .then((articles) => {
+    return Article.insertMany(articles);
+  })
+  .then((newArticle) => {
+    console.log('We just created new articles!', newArticle);
+  })
+  .catch(console.error)
+  .finally(() => {
+    process.exit();
+  });
--- middleware/auth.js
+++ middleware/auth.js
@@ -6,7 +6,7 @@ const jwt = require('jsonwebtoken');
 // Create a secret to be used to encrypt/decrypt the token
 // This can be any string value you want -- even gibberish.
 const secret =
-	process.env.JWT_SECRET || 'some string value only your app knows';
+  process.env.JWT_SECRET || 'some string value only your app knows';
 
 // Require the specific `strategy` we'll use to authenticate
 // Require the method that will handle extracting the token
@@ -15,14 +15,14 @@ const { Strategy, ExtractJwt } = require('passport-jwt');
 
 // Minimum required options for passport-jwt
 const opts = {
-	// How passport should find and extract the token from
-	// the request.  We'll be sending it as a `bearer` token
-	// when we make requests from our front end.
-	jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-	// Any secret string to use that is unique to your app
-	// We should store this in an environment variable so it
-	// isn't ever pushed to GitHub!
-	secretOrKey: secret,
+  // How passport should find and extract the token from
+  // the request.  We'll be sending it as a `bearer` token
+  // when we make requests from our front end.
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  // Any secret string to use that is unique to your app
+  // We should store this in an environment variable so it
+  // isn't ever pushed to GitHub!
+  secretOrKey: secret,
 };
 
 // Require the user model
@@ -35,19 +35,19 @@ const User = require('../models/Users');
 // extracted and decrypted by passport from the token that we get from
 // the client request!  This data (jwt_payload) will include the user's id!
 const strategy = new Strategy(opts, function (jwt_payload, done) {
-	// In the callback we run our custom code. With the data extracted from
-	// the token that we're passed as jwt_payload we'll have the user's id.
-	// Using Mongoose's `.findOneById()` method, we find the user in our database
-	User.findById(jwt_payload.id)
-		// To pass the user on to our route, we use the `done` method that
-		// that was passed as part of the callback.  The first parameter of
-		// done is an error, so we'll pass null for that argument and then
-		// pass the user doc from Mongoose.  This adds the user to the request object
-		// as request.user!
-		.then((user) => done(null, user))
-		// If there was an error, we pass it to done so it is eventually handled
-		// by our error handlers in Express
-		.catch((err) => done(err));
+  // In the callback we run our custom code. With the data extracted from
+  // the token that we're passed as jwt_payload we'll have the user's id.
+  // Using Mongoose's `.findOneById()` method, we find the user in our database
+  User.findById(jwt_payload.id)
+    // To pass the user on to our route, we use the `done` method that
+    // that was passed as part of the callback.  The first parameter of
+    // done is an error, so we'll pass null for that argument and then
+    // pass the user doc from Mongoose.  This adds the user to the request object
+    // as request.user!
+    .then((user) => done(null, user))
+    // If there was an error, we pass it to done so it is eventually handled
+    // by our error handlers in Express
+    .catch((err) => done(err));
 });
 
 // Now that we've constructed the strategy, we 'register' it so that
@@ -65,27 +65,27 @@ const requireToken = passport.authenticate('jwt', { session: false });
 // Create a function that takes the request and a user document
 // and uses them to create a token to send back to the user
 const createUserToken = (req, user) => {
-	// Make sure that we have a user, if it's null that means we didn't
-	// find the email in the database.  If there is a user, make sure
-	// that the password is correct.  For security reason, we don't want
-	// to tell the client whether the email was not found or that the
-	// password was incorrect.  Instead we send the same message for both
-	// making it much harder for hackers.
-	if (
-		!user ||
-		!req.body.password ||
-		!bcrypt.compareSync(req.body.password, user.password)
-	) {
-		const err = new Error('The provided username or password is incorrect');
-		err.statusCode = 422;
-		throw err;
-	}
-	// If no error was thrown, we create the token from user's id and
-	// return the token
-	return jwt.sign({ id: user._id }, secret, { expiresIn: 36000 });
+  // Make sure that we have a user, if it's null that means we didn't
+  // find the email in the database.  If there is a user, make sure
+  // that the password is correct.  For security reason, we don't want
+  // to tell the client whether the email was not found or that the
+  // password was incorrect.  Instead we send the same message for both
+  // making it much harder for hackers.
+  if (
+    !user ||
+    !req.body.password ||
+    !bcrypt.compareSync(req.body.password, user.password)
+  ) {
+    const err = new Error('The provided username or password is incorrect');
+    err.statusCode = 422;
+    throw err;
+  }
+  // If no error was thrown, we create the token from user's id and
+  // return the token
+  return jwt.sign({ id: user._id }, secret, { expiresIn: 36000 });
 };
 
 module.exports = {
-	requireToken,
-	createUserToken,
+  requireToken,
+  createUserToken,
 };
